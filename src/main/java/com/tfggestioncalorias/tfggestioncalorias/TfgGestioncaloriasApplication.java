badd1b26755dc7b1e0503dac8ba4d1af0d3187da
Antxon05 <antxonmoso@gmail.com>
2025-05-02 19:48:51 +0200

    Initial commit

---
+++ src/main/java/com/tfggestioncalorias/tfggestioncalorias/TfgGestioncaloriasApplication.java
@@ -0,0 +1,13 @@
+package com.tfggestioncalorias.tfggestioncalorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgGestioncaloriasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgGestioncaloriasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tfggestioncalorias/tfggestioncalorias/TfgGestioncaloriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfggestioncalorias.tfggestioncalorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgGestioncaloriasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
